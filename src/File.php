374019b4514c0b340f615022968aa44e97f49dbd
Ardyanto Hermawan <ardyanto_hermawan@yahoo.com>
2015-11-02 15:49:14 +0700

    Initial Commit

---
+++ src/File.php
@@ -0,0 +1,32 @@
+<?php
+namespace API\Lib;
+
+class File
+{
+	
+	function __construct()
+	{
+		
+	}
+
+	public function create()
+	{
+		
+	}
+
+	public function select()
+	{
+		
+	}
+
+	public function update()
+	{
+		
+	}
+
+	public function delete()
+	{
+		
+	}
+
+}
---
+++ src/User.php
@@ -0,0 +1,34 @@
+<?php
+namespace API\Lib;
+
+class User
+{
+	
+	private $userID;
+
+	function __construct()
+	{
+		
+	}
+
+	public function create()
+	{
+		
+	}
+
+	public function select()
+	{
+		
+	}
+
+	public function update()
+	{
+		
+	}
+
+	public function delete()
+	{
+		
+	}
+
+}
